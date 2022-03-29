945d77480051a81da5490cbe12b484a5408a6b41
Felipe Pardo <felipepardo90@hotmail.com>
2022-03-29 10:22:29 -0300

    Agregando al menú Nav los componentes ItemListContainer con props

---
+++ src/components/NavBar/ItemListContainer.jsx
@@ -0,0 +1,14 @@
+import { Nav } from 'react-bootstrap'
+
+const ItemListContainer = props => {
+
+
+    return(
+
+        <>
+      <Nav.Link href="#">{props.section}</Nav.Link>
+        </>
+    )
+}
+
+export default ItemListContainer
