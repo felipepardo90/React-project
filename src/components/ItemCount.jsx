f11c70fa821a51d776aab530e4bfbf3e5db1dfd1
Felipe Pardo <felipepardo90@hotmail.com>
2022-04-01 00:21:06 -0300

    Arreglando contador

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -6,7 +6,8 @@ const Counter = (initial) =>{
 const [count, setCount] = useState (initial)
 
 const incNum = () =>{
-    setCount(count + 1)  
+
+    count<5?setCount(count + 1):alert("No hay más elementos en stock")  
 }
 
 const decNum = () =>{
@@ -36,8 +37,8 @@ const ItemCount = ({stock, initial, onAdd}) => {
         <>
         <ButtonToolbar className="mb-3" aria-label="Toolbar with Button groups">
     <ButtonGroup className="me-2" aria-label="First group">
-      <Button variant="secondary" onClick={count>stock?alert("No hay más elementos en stock"):incNum}>+</Button>{' '}
-      <p max="5" min="1">Cliqueaste el botón {count} veces</p>
+      <Button variant="secondary" onClick={incNum}>+</Button>{' '}
+      <p> --- {count} --- </p>
       <Button variant="secondary" onClick={count>0?decNum:null}>-</Button>{' '}
       <Button variant="secondary" onClick={reset}>Agregar al carrito</Button>{' '}
     </ButtonGroup>
