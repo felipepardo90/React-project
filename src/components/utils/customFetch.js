5f1c62070943d7136e432604b87d9e0da72cab69
Felipe Pardo <felipepardo90@hotmail.com>
2022-04-05 14:53:21 -0300

    creación de función customFetch y utils (products)

---
+++ src/components/utils/customFetch.js
@@ -0,0 +1,9 @@
+
+const customFetch = (time, task) =>{ 
+    return new Promise ((res, rej) =>{
+        setTimeout (() => {
+            true?res(task):rej('Error')
+        }, time)
+    })}
+
+    export default customFetch
