22d3d971d26ea8bcf311ad8ca8cbfab82b3ef2ac
Felipe Pardo <felipepardo90@hotmail.com>
2022-04-11 21:42:05 -0300

    Añadiendo botón detalle ItemList

--- src/components/MainContainer/ItemList.jsx
+++ src/components/MainContainer/ItemList.jsx
@@ -5,26 +5,3 @@ export default function ItemList({ products }) {
   return products.map(p => <Item key={p.id} {...p} />);
 }
 
-// useEffect(() => {
-
-//     const pago = new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             // resolve('50')
-
-//             reject(null)
-
-//         }, 3000)
-//         //reject(null)
-//     })
-
-//     console.log(pago);
-
-//     pago.then((res) => {
-//         console.log('gracias por pagar ' + res)
-//     })
-
-//     console.log(pago)
-
-//     //sigue....
-
-// }, [])
