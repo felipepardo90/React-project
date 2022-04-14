21a85ffcd4cfa4d872e7cf8106770bb1727762c7
Felipe Pardo Netbook <felipepardo@hotmail.com>
2022-04-14 18:29:00 -0300

    estilos

--- src/components/utils/customFetch.js
+++ src/components/utils/customFetch.js
@@ -8,18 +8,3 @@ const customFetch = (time, task) => {
 export default customFetch;
 
 
-// import { useState, useEffect } from "react"
-
-// const customFetch = () =>{
-//   const [data, setData] = useState (null)
-
-//   useEffect(() => {
-//     fetch ()
-//     .then((res) => res.json())
-//     .then((data) => setData(data))
-//   }, [])
-  
-//     return [data]
-// }
-
-// export default customFetch
