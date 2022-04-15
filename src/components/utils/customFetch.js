9880887b7042166fdfb8833b3260b8eb813d4b88
Felipe Pardo <felipepardo90@hotmail.com>
2022-04-14 23:06:33 -0300

    modificando fetch

--- src/components/utils/customFetch.js
+++ src/components/utils/customFetch.js
@@ -1,7 +1,9 @@
-const customFetch = (time, task) => {
+const customFetch = (time, task, type, IDcode) => {
   return new Promise((res, rej) => {
+
+    const filter = type === 'C' ? task.filter(p=>p.category === IDcode) : task.filter(p => p.id === IDcode)
     setTimeout(() => {
-      true ? res(task) : rej("Error");
+        IDcode ? res(filter) : res(task)
     }, time);
   });
 };
