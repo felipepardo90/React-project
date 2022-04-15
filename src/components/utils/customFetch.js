e61cc363e6434fe63bede3cbbd8ca77fe34beaf6
Felipe Pardo <felipepardo90@hotmail.com>
2022-04-14 23:39:39 -0300

    último commit - se debe cambiar el customFetch

--- src/components/utils/customFetch.js
+++ src/components/utils/customFetch.js
@@ -1,7 +1,7 @@
 const customFetch = (time, task, type, IDcode) => {
   return new Promise((res, rej) => {
 
-    const filter = type === 'C' ? task.filter(p=>p.category === IDcode) : task.filter(p => p.id === IDcode)
+    const filter = type === 'C' ? task.filter(p=>p.category === IDcode) : task.find(p => p.id === parseInt(IDcode))
     setTimeout(() => {
         IDcode ? res(filter) : res(task)
     }, time);
