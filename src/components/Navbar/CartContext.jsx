63f2bb86ba20435dbcf28aae62de20e14d8b6ac8
Felipe Pardo <felipepardo90@hotmail.com>
2022-04-21 13:14:25 -0300

    funciones CartContext

---
+++ src/components/Navbar/CartContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const removeItem = (id) => setCart(cart.filter((item) => item.id !== id));
+  const clear = () => setCart([]);
+  const addItem = (item) => {
+    const indexCart = cart.findIndex((item) => item.id === item.id);
+    if (indexCart !== -1) {
+      const newCart = [...cart];
+      newCart[indexCart].count = newCart[indexCart].count + item.count;
+      setCart(newCart);
+    } else {
+      setCart([...cart, item]);
+    }
+  };
+
+  const data = { cart, addItem, removeItem, clear };
+
+  return <CartContext.Provider value={data}>{children}</CartContext.Provider>;
+};
