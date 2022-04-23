4da1439caf6fc680e04b2839945ab274a569b658
Felipe Pardo <felipepardo90@hotmail.com>
2022-04-23 20:25:51 -0300

    view products de cart widget

--- src/components/Navbar/CartWidget.jsx
+++ src/components/Navbar/CartWidget.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import s from "./NavStyles.module.css";
+import  "./_NavbarStyles.scss";
 import { Link } from "react-router-dom";
 import { CartContext } from "./CartContext";
 
@@ -10,8 +10,8 @@ export default function CartWidget() {
   return (
     <>
       <Link to="/cart">
-        <FontAwesomeIcon icon={faCartShopping} className={s.colorIcon} />
-        {cart === 0?null: <span style={{zIndex:"1", background:"red", color:"white", borderRadius:"50%", width:"10px", height:"10px", padding:"2px 5px", fontSize:"0.6em",right:"20px", bottom:"25px", position:"relative"}}>{cart.length}</span>}
+        <FontAwesomeIcon icon={faCartShopping} className="cart__widget" />
+        {cart.length === 0?null: <span className="cart__length">{cart.length}</span>}
       </Link>
     </>
   );
