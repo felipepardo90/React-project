b63d211a748fb3a107ef359e6384bee1ccc0eb3b
Felipe Pardo Netbook <felipepardo@hotmail.com>
2022-04-25 19:53:41 -0300

    cart widget styles

--- src/components/Navbar/CartWidget.jsx
+++ src/components/Navbar/CartWidget.jsx
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import  "./_NavbarStyles.scss";
+import "./_NavbarStyles.scss";
 import { Link } from "react-router-dom";
 import { CartContext } from "./CartContext";
 
 export default function CartWidget() {
-   let {cart, quantity } = useContext(CartContext)
-   console.log(quantity)
+  let { cart, quantity } = useContext(CartContext);
+  console.log(quantity);
   return (
     <>
-      <Link to="/cart">
-        <FontAwesomeIcon icon={faCartShopping} className="cart__widget" />
-        {cart.length > 0 && <span className="cart__length">{quantity}</span>}
+      <Link to="/cart" style={{textDecoration:"none"}}>
+        <FontAwesomeIcon icon={faCartShopping} className="cart__widget"/>
+          {cart.length > 0 && (
+            <span className="cart__length">
+              <p>{quantity}</p>
+            </span>
+          )}
+        
       </Link>
     </>
   );
