1605099efb2295884335351d5258b25455a013ad
Felipe Pardo <felipepardo90@hotmail.com>
2022-04-28 19:34:34 -0300

    trayendo productos desde firebase

--- src/components/MainContainer/ItemListContainer.jsx
+++ src/components/MainContainer/ItemListContainer.jsx
@@ -1,44 +1,57 @@
-import React, {useState, useEffect} from "react"
-import { Container } from 'react-bootstrap'
-import ItemList from './ItemList'
-import customFetch from "../utils/customFetch"
-import productsJson from '../utils/products.json'
-import './ItemListContainer.scss'
-import { useParams } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import ItemList from "./ItemList";
+import customFetch from "../utils/customFetch";
+import productsJson from "../utils/products.json";
+import "./ItemListContainer.scss";
+import { useParams } from "react-router-dom";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 export default function ItemListContainer() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
 
-    const [postres, setPostres] = useState([])
-    const [loading, setLoading] = useState(true)
-    const {category} = useParams()
+  useEffect(()=>{
 
+const db = getFirestore()
+const productsRef = collection(db, "products")
+getDocs(productsRef).then(res=>{
+  console.log(res.docs)
+  setProducts(res.docs.map(p=>({id:p.id, ...p.data()})))
+})
+console.log(products)
 
-    useEffect(()=>{
-      customFetch (2000, productsJson, 'C', category)
-      .then(res => setPostres(res))
-      .catch(error => console.log(error))
-      .finally(()=>{
-        setLoading(false)
-      })
-    }, [category])
+  },[])
 
+  // useEffect(() => {
+  //   // // const db = getFirestore();
 
-
-    return(
-
-<>
-
-{ loading ? (<div className="spinner-border text-danger" role="status">
-  <span className="visually-hidden">Loading...</span>
-</div>) : 
-(<Container className="main-container">
-
-  <ItemList products={postres} />
     
-  </Container>)}
-  
-  </>
 
-    )
+  //   customFetch(2000, productsJson, "C", category)
+  //     .then((res) => setPostres(res))
+  //     .catch((error) => console.log(error))
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+
+  // }, [category]);
+
+  return (
+    <>
+      {/* {loading ? (
+        <div className="spinner-border text-danger" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      ) : (
+        <Container className="main-container">
+          <ItemList products={products} />
+        </Container>
+      )} */}
+      <Container className="main-container">
+          <ItemList products={products} />
+        </Container>
+    </>
+  );
 }
-
