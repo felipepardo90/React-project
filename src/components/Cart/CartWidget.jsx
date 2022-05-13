798f37e631df7498ff65543eb16dbe3d7c2faa8d
Felipe Pardo <felipepardo90@hotmail.com>
2022-05-13 13:36:47 -0300

    styles fixed

--- src/components/Cart/CartWidget.jsx
+++ src/components/Cart/CartWidget.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import "../Navbar/_NavbarStyles.scss";
