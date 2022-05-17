ad0529baea571e7a2e2f6c9332243470220e8ff8
Felipe Pardo <felipepardo90@hotmail.com>
2022-05-17 00:24:57 -0300

    404

--- src/components/utils/NotFound.jsx
+++ src/components/utils/NotFound.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const NotFound = () => (
 
-    <h1>404</h1>
+    <body>
+    <div id="message">
+      <h2>404</h2>
+      <h1>Página no encontrada</h1>
+      <p>El archivo especificado no fue encontrado en este sitio. Por favor, revise los errores en la dirección URL e inténtelo nuevamente</p>
+      <h3>¿Por qué estoy viendo esto?</h3>
+      <p>Esta página fue generada porque no se encuentra la ruta que está buscando.</p>
+    </div>
+  </body>
 )
   
   
