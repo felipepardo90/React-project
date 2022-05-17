bb758b3f8bc78cd733ffd8925d7a7343e13de6c0
Felipe Pardo <felipepardo90@hotmail.com>
2022-05-16 22:15:50 -0300

    validación Formulario

--- src/components/Cart/FormOrder.jsx
+++ src/components/Cart/FormOrder.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 const FormOrder = ({ data, settings }) => {
+
+  const [openModal, setOpenModal] = useState(true);
+  const close = () => setOpenModal(false);
+
   return (
     <>
+    <Modal
+        show={openModal}
+        onHide={close}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>¡Atención!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h2>Está por finalizar su compra</h2>
+          <p>Esperamos que pueda disfrutar nuestros productos, desde la comodidad de su hogar, con nuestros envíos a domicilio.</p>
+          <p>Para ello, deberá completar el siguiente formulario. Sus datos no serán proporcionados a terceros.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={close}>
+            Entendido
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <div className="container-form">
         <div className="form-box">
           <div className="image-box"></div>
@@ -14,37 +40,39 @@ const FormOrder = ({ data, settings }) => {
               }}
             >
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
+                <label className="form-label">
                   Nombre/s
                 </label>
                 <input
                   className="form-control field"
                   type="text"
-                  placeholder="nombre/s"
+                  placeholder="Felipe"
                   value={data.name}
                   onChange={(e) => {
                     settings.setName(e.currentTarget.value);
                   }}
+                  pattern="^([a-zA-Z ]){2,30}$"
                   required
                 />
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
+                <label className="form-label">
                   Apellido
                 </label>
                 <input
                   className="form-control field"
                   type="text"
-                  placeholder="apellido"
+                  placeholder="Pardo"
                   value={data.lastname}
                   onChange={(e) => {
                     settings.setLastname(e.currentTarget.value);
                   }}
+                  pattern="^([a-zA-Z ]){2,30}$"
                   required
                 />
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
+                <label  className="form-label">
                   Correo
                 </label>
                 <input
@@ -55,17 +83,18 @@ const FormOrder = ({ data, settings }) => {
                   onChange={(e) => {
                     settings.setEmail(e.currentTarget.value);
                   }}
+                  pattern="[^@\s]+@[^@\s]+" 
                   required
                 />
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
+                <label  className="form-label">
                   Dirección
                 </label>
                 <input
                   className="form-control field"
                   type="text"
-                  placeholder="address"
+                  placeholder="Avda Belgrano 2020"
                   value={data.address}
                   onChange={(e) => {
                     settings.setAddress(e.currentTarget.value);
@@ -74,17 +103,18 @@ const FormOrder = ({ data, settings }) => {
                 />
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
+                <label className="form-label">
                   Teléfono
                 </label>
                 <input
                   className="form-control field"
-                  type="text"
-                  placeholder="teléfono"
+                  type="tel"
+                  placeholder="000 000 0000"
                   value={data.phone}
                   onChange={(e) => {
                     settings.setPhone(e.currentTarget.value);
                   }}
+                  pattern="[0-9]{3} [0-9]{3} [0-9]{4}"
                   required
                 />
               </div>
